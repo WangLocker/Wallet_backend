03708e32b0bb80896d5086e6c90b554984f22234
WangLocker <1057857414@qq.com>
2024-12-03 20:32:42 +0800

    delete some sql functions in mapper

--- src/main/java/com/example/payment/mapper/UserMapper.java
+++ src/main/java/com/example/payment/mapper/UserMapper.java
@@ -12,25 +12,13 @@ public interface UserMapper {
     User getUserById(Integer id);
     @Select("SELECT * FROM users WHERE email = #{email}")
     User getUserByEmail(String email);
-
     @Select("SELECT * FROM users WHERE phone = #{phone}")
     User getUserByPhone(String phone);
-
     @Select("SELECT * FROM users WHERE ssn = #{ssn}")
     User getUserBySsn(String ssn);
-
-    @Select("SELECT * FROM users")
-    List<User> getAllUsers();
-
     @Insert("INSERT INTO users (name, ssn, phone, email,password, is_email_verified, is_phone_verified) " +
             "VALUES (#{name}, #{ssn}, #{phone}, #{email},#{password}, #{isEmailVerified}, #{isPhoneVerified})")
     @Options(useGeneratedKeys = true, keyProperty = "id")
     void insertUser(User user);
 
-    @Update("UPDATE users SET name = #{name}, phone = #{phone}, email = #{email} " +
-            "WHERE id = #{id}")
-    void updateUser(User user);
-
-    @Delete("DELETE FROM users WHERE id = #{id}")
-    void deleteUserById(Integer id);
 }
